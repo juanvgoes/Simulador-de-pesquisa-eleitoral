f4798f1f5fd63934d1061271334d380820295d7c
Juan <juan.v.goes@gmail.com>
2026-09-09 16:15:38 -0300

    Correção de bug ao errar input de adicionar candidato, adição de porcentagem de votos e formatação da porcentagem, além da lógica para ordenar o print de candidatos na ordem decrescente de porcentagem de votos

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from ast import While
 from time import sleep
 #falta muita coisa ainda visse
 lista = []
+lista_porcentagem = []
 encerrar = False
 
 print('Pesquisa eleitoral')
@@ -37,9 +39,8 @@ while True:
 
     lista.append([candidato, vice, partido, votos])
 
-    decisao_repetir = input('Deseja adicionar outro candidato? [S/N] ')
-
     while True:
+        decisao_repetir = input('Deseja adicionar outro candidato? [S/N] ')
         if decisao_repetir.lower().strip() == 's' or decisao_repetir.lower().strip() == 'sim':
             print('\n-------------------------------\n')
             break
@@ -57,9 +58,28 @@ while True:
 print('As informações recebidas foram as seguites:')
 for c in range(len(lista)):
     print(f'Candidato(a): {lista[c][0]}')
-    for c in range(3):
-        print(f'Vice: {lista[c][1]}')
-        print(f'Partido: {lista[c][2]}')
-        print(f'Votos até o momento: {lista[c][3]}')
+    print(f'Vice: {lista[c][1]}')
+    print(f'Partido: {lista[c][2]}')
+    print(f'Votos até o momento: {lista[c][3]}')
     print('----------------------------------')
+
+total_votos = 0
+for c in range(len(lista)):
+    total_votos += lista[c][3]
+
+print(f'Total de votos: {total_votos}')
+
+for c in range(len(lista)):
+    porcentagem = (lista[c][3] / total_votos) * 100
+    lista_porcentagem.append(porcentagem)
+    lista[c].append(porcentagem)
+
+i = c = 0
+while i < len(lista):
+    if lista[c][4] == lista_porcentagem[i]:
+        print(f'{lista[c][0]} tem {lista[c][4]:.2f}% dos votos, e está em {i + 1} lugar')
+        i += 1
+        c = 0
+    else:
+        c += 1
 
